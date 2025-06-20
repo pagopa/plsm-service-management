05aab65c0023f65009ad7362e8b1d9add5856282
LoFrance <lorenzo.franceschini@pagopa.it>
2025-06-20 13:56:09 +0200

    Work in progress

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -1,3 +1,13 @@
 data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
+
+data "azurerm_user_assigned_identity" "identity_prod_ci" {
+  name                = "${local.project}-functions-admin-github-ci-identity"
+  resource_group_name = local.identity_resource_group_name
+}
+
+data "azurerm_user_assigned_identity" "identity_prod_cd" {
+  name                = "${local.project}-functions-admin-github-cd-identity"
+  resource_group_name = local.identity_resource_group_name
+}
