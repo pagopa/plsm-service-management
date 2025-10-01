ecd1969d615a70dc93e1f699d420f39b2898c374
LoFrance <lorenzo.franceschini@pagopa.it>
2025-10-01 19:17:38 +0200

    Infra: remove unnecessary comment

--- infra/resources/_modules/function_app/output.tf
+++ infra/resources/_modules/function_app/output.tf
@@ -6,15 +6,6 @@ output "func_details" {
 
 output "function_app_id" {
   description = "The ID of the Azure Function App created by the inner module."
-  # Prendiamo l'ID dall'output del modulo "azurerm_linux_function_app"
-  # che hai dichiarato nel main.tf di questo modulo.
-  value = module.azurerm_linux_function_app.function_app.function_app.id
+  value       = module.azurerm_linux_function_app.function_app.function_app.id
 }
-# OUTPUT del modulo: 
-# https://github.com/pagopa-dx/terraform-azurerm-azure-function-app/blob/main/outputs.tf
-# module.onboarding_function
-# └── func_details (Questo è un oggetto)
-#     └── function_app (Questo è il vero oggetto della Function App)
-#         ├── id  <-- L'attributo che ti serve è qui dentro!
-#         ├── name
-#         └── ...altri attributi
+
