65573eda954045bd37465ca1e555c17f7bfaca20
Lorenzo Franceschini <78347349+LoFrance@users.noreply.github.com>
2025-10-09 23:21:29 +0200

    fix endpoint (#64)

--- apps/sm-onboarding-fn/Health/index.ts
+++ apps/sm-onboarding-fn/Health/index.ts
@@ -10,7 +10,7 @@ const app = express();
 secureExpressApp(app);
 
 // Add express route
-app.get('/v1/health', Info());
+app.get('/', Info());
 
 const azureFunctionHandler = createAzureFunctionHandler(app);
 
