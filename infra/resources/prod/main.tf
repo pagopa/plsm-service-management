0ba661d0bfbf5d277cd18f3272c99f724ae24e24
LoFrance <lorenzo.franceschini@pagopa.it>
2025-10-10 15:03:03 +0200

    Add PEP onboarding function

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -155,6 +155,31 @@ module "onboarding_function" {
   depends_on = [module.azure_core_infra]
 }
 
+resource "azurerm_private_endpoint" "onboarding_func_to_selc_eventhub" {
+  name                = "plsm-p-itn-selc-evhns-pep-01"
+  location            = "Italy North"
+  resource_group_name = "plsm-p-itn-network-rg-01"
+
+  # ID della subnet, ora completo con il nome corretto
+  subnet_id = "/subscriptions/c703d239-22b7-4d1a-9433-145daa884c10/resourceGroups/plsm-p-itn-network-rg-01/providers/Microsoft.Network/virtualNetworks/plsm-p-itn-common-vnet-01/subnets/plsm-p-itn-pep-snet-01"
+
+  private_service_connection {
+    name = "plsm-p-itn-selc-evhns-psc-01"
+
+    private_connection_resource_id = "/subscriptions/${var.eventhub_subscription_id}/resourceGroups/selc-p-event-rg/providers/Microsoft.EventHub/namespaces/selc-p-eventhub-ns"
+
+    is_manual_connection = true
+
+    request_message   = "Connessione da Azure Function 'plsm-p-itn-onboarding-func-01' per Onboarding"
+    subresource_names = ["namespace"]
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = ["/subscriptions/${var.subscription_id}/resourceGroups/plsm-p-itn-network-rg-01/providers/Microsoft.Network/privateDnsZones/privatelink.servicebus.windows.net"]
+  }
+}
+
 # App Service
 resource "azurerm_resource_group" "apps_rg" {
   name     = "plsm-p-itn-apps-rg-01"
---
+++ infra/resources/prod/variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+  description = "Subscription ID of PLSM"
+  type        = string
+}
+
+variable "eventhub_subscription_id" {
+  description = "Subscription ID of the target EventHub"
+  type        = string
+}
