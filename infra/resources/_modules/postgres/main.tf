97400fbd222b737200a3fa7187e5a2c0b3b057f4
LoFrance <lorenzo.franceschini@pagopa.it>
2025-11-19 15:49:43 +0100

    Rever postgresql to 1.0.7 DX module

--- infra/resources/_modules/postgres/main.tf
+++ infra/resources/_modules/postgres/main.tf
@@ -1,6 +1,6 @@
 module "postgres" {
   source  = "pagopa-dx/azure-postgres-server/azurerm"
-  version = "~> 2.0.0"
+  version = "~> 1.0.7"
 
   resource_group_name = var.resource_group_name
 
@@ -12,9 +12,6 @@ module "postgres" {
     instance_number = "01"
   }
   pgbouncer_enabled = false
-  create_replica    = false
-  # replica_location           = "Italy North"
-  high_availability_override = false
 
   subnet_pep_id                        = var.subnet_pep_id
   private_dns_zone_resource_group_name = var.private_dns_zone_resource_group_name
