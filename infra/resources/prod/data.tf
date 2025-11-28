266ad2be5130d6d672955063651e0b3da8f874b6
LoFrance <lorenzo.franceschini@pagopa.it>
2025-11-28 10:33:25 +0100

    Add 2 keys

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -122,6 +122,11 @@ data "azurerm_key_vault_secret" "fe_smcr_slack_call_management_hook_prod" {
 
 # Function Onboarding
 
+data "azurerm_key_vault_secret" "ocp_apim_subscription_key" {
+  name         = "fa-onboarding-ocp-apim-subscription-key"
+  key_vault_id = module.azure_core_infra.common_key_vault.id
+}
+
 data "azurerm_key_vault_secret" "sc_contracts_conn_string" {
   name         = "fa-onboarding-sc-contracts-conn-string"
   key_vault_id = module.azure_core_infra.common_key_vault.id
@@ -164,6 +169,11 @@ data "azurerm_key_vault_secret" "slack_webhook_onboarding_pagopa" {
 
 
 # Function Ask Me Bot
+
+data "azurerm_key_vault_secret" "askmebot_ocp_apim_subscription_key" {
+  name         = "fa-askmebot-ocp-apim-subscription-key"
+  key_vault_id = module.azure_core_infra.common_key_vault.id
+}
 
 data "azurerm_key_vault_secret" "askmebot_slack_bot_token" {
   name         = "fa-askmebot-slack-bot-token"
