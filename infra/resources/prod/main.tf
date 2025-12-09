9d90f6c4277687347758d9df9a9f321de655a028
LoFrance <lorenzo.franceschini@pagopa.it>
2025-12-09 22:14:18 +0100

    Change storage redundancy

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -492,7 +492,7 @@ resource "azurerm_storage_account" "storage_marco_ext_001" {
   resource_group_name      = azurerm_resource_group.ext_rg.name
   location                 = azurerm_resource_group.ext_rg.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "production"
