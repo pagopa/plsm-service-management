e736eaeb07418da4ee6502f23f2a531716b2667f
Mandeep Singh <sonomandeep@gmail.com>
2025-12-19 11:01:13 +0100

    feat(smcr): update logs header with search and filters

--- apps/sm-fe-smcr/app/dashboard/logs/layout.tsx
+++ apps/sm-fe-smcr/app/dashboard/logs/layout.tsx
@@ -1,4 +1,4 @@
-import { ActivityIcon } from "lucide-react";
+import { LogsHeader } from "@/components/logs/logs-header";
 
 type Props = {
   children: React.ReactNode;
@@ -7,12 +7,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className="bg-neutral-50 h-screen w-full overflow-hidden p-3 flex flex-col gap-3">
-      <header className="">
-        <div className="inline-flex gap-2 items-center">
-          <ActivityIcon className="size-3.5 opacity-60" />
-          <p className="font-medium text-lg">Logs</p>
-        </div>
-      </header>
+      <LogsHeader />
 
       {children}
     </div>
