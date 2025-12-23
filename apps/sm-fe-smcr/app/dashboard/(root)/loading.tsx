33251c6e2ddd1f08fa965ec508708d25e3b4fec4
Niccolò Naso <90309270+LordNik10@users.noreply.github.com>
2025-12-23 09:52:36 +0100

    [SMION-113] Come sm voglio migliorare la dashboard di SMCR (#181)
    - Aggiunti colori per ogni prodotto più riconoscibile
    - Rimozione del grafico mocckato
    - Aggiunta del grafico a torta per rappresentare al meglio il numero
    degli onboarding ed il periodo di riferimento
    - Aggiunta "Last month" per avere un idea dello storico
    
    ---------
    
    Co-authored-by: Niccolò Naso <niccolo.naso@bitrock.it>

--- apps/sm-fe-smcr/app/dashboard/(root)/loading.tsx
+++ apps/sm-fe-smcr/app/dashboard/(root)/loading.tsx
@@ -14,21 +14,34 @@ export default function DashboardLoading() {
               <Skeleton className="h-6 w-16 rounded-full" />
               <Skeleton className="h-4 w-12" />
             </div>
+            <Skeleton className="mt-4 h-3 w-32" />
           </div>
         ))}
       </section>
 
       <section className="rounded-3xl border border-black/5 bg-white/80 p-6 shadow-sm">
         <Skeleton className="h-5 w-28" />
-        <Skeleton className="mt-4 h-48 w-full" />
-        <div className="mt-6 flex justify-between text-xs text-muted-foreground">
+        <Skeleton className="mt-4 h-[500px] w-full" />
+        <div className="w-full flex flex-row justify-center mt-4">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-row gap-6">
+              {[...Array(3)].map((_, index) => (
+                <Skeleton key={index} className="h-4 w-10" />
+              ))}
+            </div>
+            <div className="flex flex-row gap-6 justify-evenly">
+              {[...Array(2)].map((_, index) => (
+                <Skeleton key={index} className="h-4 w-10" />
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* <div className="mt-6 flex justify-between text-xs text-muted-foreground">
           {[...Array(15)].map((_, index) => (
             <Skeleton key={index} className="h-4 w-10" />
           ))}
-        </div>
+        </div> */}
       </section>
     </div>
   );
 }
-
-
