1dd32c442056ca8ccfa91e1913803f75cd667544
LoFrance <lorenzo.franceschini@pagopa.it>
2026-03-04 14:47:33 +0100

    Add infra-cd to bootstrapper

--- infra/bootstrapper/prod/main.tf
+++ infra/bootstrapper/prod/main.tf
@@ -77,6 +77,9 @@ module "azure-github-environment-bootstrap" {
   opex_resource_group_id             = data.azurerm_resource_group.opex_rg.id
   repository                         = local.repository
 
+  additional_resource_group_ids = [
+    data.azurerm_resource_group.apps_rg.id,
+  ]
 
   tags = local.tags
 }
