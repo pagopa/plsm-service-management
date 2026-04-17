f1a023c78011dd7cb1c7b58032e98506fe5b6acb
LoFrance <lorenzo.franceschini@pagopa.it>
2026-04-17 22:58:49 +0200

    Add storage and locals for crm

--- infra/resources/prod/storage.tf
+++ infra/resources/prod/storage.tf
@@ -13,3 +13,19 @@ resource "azurerm_storage_account" "storage_marco_ext_001" {
     environment = "production"
   }
 }
+
+resource "azurerm_storage_account" "diagnostics_storage" {
+  name                     = "plsmpitdiagst001"
+  resource_group_name      = azurerm_resource_group.fn_rg.name
+  location                 = azurerm_resource_group.fn_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "crm_diagnostics" {
+  name                  = "crm-diagnostics"
+  storage_account_id    = azurerm_storage_account.diagnostics_storage.id
+  container_access_type = "private"
+}
