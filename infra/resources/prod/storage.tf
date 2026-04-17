ede437d25e044407ab9c87fcf5d0f61a72f16618
Lorenzo Franceschini <78347349+LoFrance@users.noreply.github.com>
2026-04-17 23:10:51 +0200

    [INFA] Add storage and locals for crm (#271)

--- infra/resources/prod/storage.tf
+++ infra/resources/prod/storage.tf
@@ -9,7 +9,21 @@ resource "azurerm_storage_account" "storage_marco_ext_001" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags = {
-    environment = "production"
-  }
+  tags = local.tags
+}
+
+resource "azurerm_storage_account" "diagnostics_storage" {
+  name                     = "plsmpitdiagst001"
+  resource_group_name      = azurerm_resource_group.fn_rg.name
+  location                 = azurerm_resource_group.fn_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "crm_diagnostics" {
+  name                  = "crm-diagnostics"
+  storage_account_id    = azurerm_storage_account.diagnostics_storage.id
+  container_access_type = "private"
 }
