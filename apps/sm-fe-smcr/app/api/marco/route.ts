83e12e5fbc48e321477ccfa9f6d91e0ac66812f7
Lorenzo Franceschini <78347349+LoFrance@users.noreply.github.com>
2026-05-26 10:09:04 +0200

    Eliminato route di prova marco

--- apps/sm-fe-smcr/app/api/marco/route.ts
+++
@@ -1,5 +0,0 @@
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get("id")
-    return new Response(id || "No ID provided", {status: 200})
-}
